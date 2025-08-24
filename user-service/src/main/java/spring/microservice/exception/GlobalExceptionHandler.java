833ab6473a95be77b289a248856b70348b46e513
Felim3012 <morbius572@gmail.com>
2025-08-24 17:37:40 +0700

    change global exception

--- user-service/src/main/java/spring/microservice/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/spring/microservice/exception/GlobalExceptionHandler.java
@@ -6,33 +6,30 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import reactor.core.publisher.Mono;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import spring.microservice.dto.response.BaseResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AuthenticationException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public Mono<Map<String, Object>> handleAuthenticationException(AuthenticationException ex) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("status", HttpStatus.UNAUTHORIZED.value());
-        response.put("error", "Unauthorized");
-        response.put("message", ex.getMessage());
-        response.put("timestamp", LocalDateTime.now().toString());
-        return Mono.just(response);
+    public Mono<BaseResponse<String>> handleAuthenticationException(AuthenticationException ex) {
+        return Mono.just(
+                BaseResponse.<String>builder()
+                        .message(ex.getMessage())
+                        .payload("Unauthorized")
+                        .build()
+        );
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Mono<Map<String, Object>> handleGeneralException(Exception ex) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        response.put("error", "Internal Server Error");
-        response.put("message", ex.getMessage());
-        response.put("timestamp", LocalDateTime.now().toString());
-        return Mono.just(response);
+    public Mono<BaseResponse<String>> handleGeneralException(Exception ex) {
+        return Mono.just(
+                BaseResponse.<String>builder()
+                        .message(ex.getMessage())
+                        .payload("Internal server error")
+                        .build()
+        );
     }
 }
